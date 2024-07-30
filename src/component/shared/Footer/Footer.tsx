1d969ef9c43288225c3c8724df91531d5612ac1c
Shihabuddin9 <shihab.bd.pk@gmail.com>
2024-07-30 20:20:46 +0600

    add background color of footer

--- src/component/shared/Footer/Footer.tsx
+++ src/component/shared/Footer/Footer.tsx
@@ -4,7 +4,7 @@ import { Container, Typography } from "@mui/material";
 
 const Footer = () => {
     return (
-        <Container maxWidth="xl" sx={{ paddingX: '15px !important', marginTop: '60px' }}>
+        <Container maxWidth="xl" sx={{ paddingX: '15px !important', marginTop: '60px', backgroundColor: '#f3f3f3', paddingY: 2 }}>
             <Typography textAlign='center'>Copyright © 2024 picVault.com</Typography>
         </Container>
     );
