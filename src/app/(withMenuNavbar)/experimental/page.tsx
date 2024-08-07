566731565e160aecfaacab964961e252fcd6d1fc
Shihabuddin9 <shihab.bd.pk@gmail.com>
2024-08-08 00:25:47 +0600

    film archival experimental animals fashionBeauty people spirituality businessWork foodDrink healthWellness renders currentEvents

---
+++ src/app/(withMenuNavbar)/experimental/page.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import HigherOrderFunction from '@/component/utils/HigherOrderFunction/HigherOrderFunction';
+import { Box } from '@mui/material';
+import WithMenuNavbarImageGallery from '@/component/ui/WithMenuNavbarImageGallery/WithMenuNavbarImageGallery';
+
+interface Photo {
+    category: string;
+    img: string; // URL or path to the image
+    title: string; // Title or description of the image
+    _id: string;
+}
+
+interface FilterPhotoProps {
+    filterPhoto: Photo[];
+}
+
+const Travel: React.FC<FilterPhotoProps> = ({ filterPhoto }) => {
+    return (
+        <Box>
+            <h1>Experimental</h1>
+            <WithMenuNavbarImageGallery filterPhoto={filterPhoto} />
+        </Box>
+    );
+};
+
+// Wrap Illustrations component with HigherOrderFunction
+const IllustrationsWithFilter = HigherOrderFunction('Travel', Travel);
+export default IllustrationsWithFilter;
